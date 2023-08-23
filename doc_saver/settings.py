a2af4dc40e5e40fde8b761762494dc592a33e6a5
jiyeon-kim-ds <jiyeon.kim.ds@gmail.com>
2023-08-23 22:41:29 +0900

    Changed secret key into env var

--- doc_saver/settings.py
+++ doc_saver/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8(3*a@#h6pcjvfjgoffo*mva#z46%bt22yhfn@p$6@z@87g^()'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
