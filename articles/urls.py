5115c4c3ff5f20e5df50e214e849a4ada5607f68
jiyeon-kim-ds <jiyeon.kim.ds@gmail.com>
2023-09-01 15:02:42 +0900

    ADD URL configurations

---
+++ articles/urls.py
@@ -0,0 +1,15 @@
+from django.urls    import path, include
+from rest_framework import routers
+
+from articles.views import ArticleViewSet
+
+
+app_name = 'articles'
+
+article_router = routers.SimpleRouter()
+
+article_router.register('', ArticleViewSet, basename='Article')
+
+urlpatterns = [
+    path('', include(article_router.urls))
+]
